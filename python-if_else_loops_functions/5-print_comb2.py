044d6993eb3151f351bec5bdaced8e1b60a7f0ae
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-25 12:05:02 -0500

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,8 +3,7 @@
 i = 0
 
 for i in range(0, 100):
-    i + 1
     if i >= 10:
-        print(i)
+        print(f'{i}'.format(i + 1))
     if i < 10:
-        print(f'0{i}')
+        print(f'0{i}'.format(i + 1))
