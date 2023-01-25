8fba503f2f1a9e7f02f00045f13cbed3c7566052
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-25 13:13:14 -0500

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,7 @@ for i in range(0, 10):
     for f in range(1, 10):
         if i >= f:
             continue
+        elif i == 8 and f == 9:
+            print(f'{i}{f}'.format(i, f))
         else:
             print(f'{i}{f}, '.format(i, f), end='')
