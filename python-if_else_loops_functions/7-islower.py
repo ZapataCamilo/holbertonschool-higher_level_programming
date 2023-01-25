519ab48364b99a1c393c026d928f576c459fa011
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-25 13:28:11 -0500

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,9 @@
 
 def islower(c):
     convert = ord(c)
-    for i in range(convert):
+    for i in range(97, 122):
         if convert == i:
             return True
         else:
             return False
+            
