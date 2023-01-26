0cc4bda8ce76ad1f281bf4b5cacfeaa89a077b93
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-26 07:17:26 -0500

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i in range(len(str)):
+        if str[i] >= 'a' and str[i] <= 'z':
+            convert = str[i]
+            convert = ord(convert)
+            convert = convert-32
+            convert = chr(convert)
+            str = str[:i] + convert + str[i + 1:]
+    print(str)
