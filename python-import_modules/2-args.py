98932d37ba7eb2770118060ea0775cfaa6d26253
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-27 14:48:04 -0500

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,12 +5,16 @@ if __name__ == '__main__':
     str = argv
     str_len = len(str)
 
-    if str:
-        if str == str[0]:
-            print('0 argument.'.format(str_len -1))
-        if str[1]:
+    if str_len:
+        if str_len == 1:
+            print('0 arguments.')
+        elif str_len == 2:
+            print('{} argument:'.format(str_len - 1))
+        elif str_len > 2:
             print('{} arguments:'.format(str_len - 1))
         for i in range(1, str_len):
             i == i + 1
             if str[i]:
                 print('{}: {}'.format(i, str[i]))
+    elif str[0]:
+        print('{} arguments.'.format(str_len))
