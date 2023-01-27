85e0d30ff6d736a049de766b733b6a6a55cc06dd
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-27 13:20:44 -0500

    task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
 
     if str:
         if str == str[0]:
-            print('0 arguments.'.format(str_len -1))
+            print('0 argument.'.format(str_len -1))
         if str[1]:
-            print('{} arguments:'.format(str_len - 1))
+            print('{} argument:'.format(str_len - 1))
         for i in range(1, str_len):
             i == i + 1
             if str[i]:
