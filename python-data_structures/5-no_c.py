5060aad8c97ef6ad7eeeb0d5f5efda223415ada7
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-31 12:47:31 -0500

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        my_string[i] = ord(my_string[i])
-        if my_string[i] == 67 or my_string[i] == 99:
-            my_string[i] = chr(my_string[i])
-            my_string[i].pop(i)
-            return my_string
+    new_str = ''
+    for no_letter in my_string:
+        if no_letter == 'c' or no_letter == 'C':
+            no_letter = ''
+        new_str += no_letter
+    return new_str
