afa69c04377f13650f627ce208604c195602f85c
ZapataCamilo <zapatacamilo86@gmail.com>
2023-01-31 15:13:40 -0500

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    first_dg = sentence[0]
-    if not sentence:
-        return None
-    return length, first_dg
+    if length:
+        return length, sentence[0]
+    elif not length:
+        return length, None
