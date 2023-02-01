d21fae0b07be02659e7bdae1777c9cfe6505dfd1
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-01 06:18:29 -0500

    task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     myList = []
 
     for i in range(len(my_list)):
-        if i % 2 == 0:
+        if my_list[i] % 2 == 0:
             myList.append(True)
         else:
             myList.append(False)
