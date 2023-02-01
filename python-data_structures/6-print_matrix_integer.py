2524619e96bb4594fe88e8afe134c7f6f7297aaf
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-01 12:21:30 -0500

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
         matrix_final = '\n'.join([' '.join(['{:d}\
-    '.format(item) for item in row]) for row in matrix])
+'.format(item) for item in row]) for row in matrix])
         print(matrix_final)
