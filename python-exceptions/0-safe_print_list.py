735f85079088257d940c0dd5c2eaca9a654bb740
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-03 11:17:03 -0500

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     elments = 0
     try:
         for i in range(x):
-           print(my_list[i], end='')
-           elments += 1
+            print(my_list[i], end='')
+            elments += 1
     except TypeError:
         pass
     finally:
