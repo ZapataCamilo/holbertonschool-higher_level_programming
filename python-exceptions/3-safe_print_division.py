c84f6b92aa85cd4f8629a4fbc651bbc3cf75515f
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-03 15:09:03 -0500

    task 4

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        result = None
+        print('Error! tiene un 0')
     finally:
         print('Inside result: {}'.format(result))
         return result
