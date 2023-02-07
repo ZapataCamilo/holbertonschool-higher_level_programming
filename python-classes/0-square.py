2a6075b70af0528c3a3445ae28372b3e235606e4
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-07 05:46:37 -0500

    task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """In this code we are going to create a empty class call Square"""
-# Square is the empty class
 
 
 class Square:
+    """Square is the empty class"""
+
     def __init__(self) -> None:
         pass
