640e016741987819629cb0c434104c4d9750e821
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-07 06:23:40 -0500

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""In this code we are going to create class call Square"""
+
+
+class Square:
+    """This class instance size"""
+
+    def __init__(self, __size) -> None:
+        self.__size = 3
