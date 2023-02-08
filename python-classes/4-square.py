9528b84f03960abfe46b2a583eba3b93a9598f4c
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-08 06:20:06 -0500

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""In this code we are going to create the class called Plaza and show an
+error if it is not int"""
+
+
+class Square:
+    """This class instance size"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        Area = self.size * self.size
+        return Area
