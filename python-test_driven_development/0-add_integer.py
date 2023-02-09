01aca81ec0fd849e5ec026832bcb5bcdeaab29c1
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-09 06:25:02 -0500

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,8 +5,8 @@ def add_integer(a, b=98):
         20
         >>> add_integer(14, 30)
         44
-        >>> add_integer(int(10.0), int(20.0))
-        30
+        >>> add_integer(10.0, 20.0)
+        30.0
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
