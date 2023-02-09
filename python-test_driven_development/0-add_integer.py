9744f78554cf1f15871d9b2de5c6fb51dd5d2f58
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-09 12:44:43 -0500

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,19 +4,10 @@
 
 def add_integer(a, b=98):
     """Adds two integers"""
-    """
-    >>> add_integer(None)
-    Traceback (most recent call last):
-    ...
-    TypeError: a must be an integer
-    """
+
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     elif not isinstance(b, int) and not isinstance(b, float):
         raise TypeError('b must be an integer')
 
     return int(a) + int(b)
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testfile
