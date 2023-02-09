11f84a4406bbebb2cb3eb0a2a6072df6d878e574
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-09 13:24:22 -0500

    task 0

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """function that print"""
 
+
 def say_my_name(first_name, last_name=""):
+    """if the arguments are not str"""
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
