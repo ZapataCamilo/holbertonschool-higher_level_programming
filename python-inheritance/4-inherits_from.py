8a3b87c3ddf6280c93029f4bdbbc1496203b0b19
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-16 11:28:14 -0500

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
 returns True if the object is an instance of a class that
-inherited (directly or indirectly)
-from the specified class
+inherited from the specified class
 """
 
 
