f387e063dd79d552b14314f534f593aa89f8cdf9
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-16 13:56:22 -0500

    task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,6 +5,7 @@ Write a class BaseGeometry
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """
       class Rectangle that inherits from
@@ -15,9 +16,9 @@ class Rectangle(BaseGeometry):
         self.__height = height
         super().integer_validator("width", width)
         super().integer_validator("height", height)
-    
+
     def area(self):
         return self.__width * self.__height
-    
+
     def __str__(self) -> str:
         return f"[Rectangle] {self.__width}/{self.__height}"
