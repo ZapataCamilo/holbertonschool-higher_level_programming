7fbdfa2e84a30958641e9878cbda04a55fb8367a
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-16 12:55:48 -0500

    task 7

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -5,6 +5,8 @@ Write a class BaseGeometry
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+bg = BaseGeometry()
+
 class Rectangle(BaseGeometry):
     """
       class Rectangle that inherits from
@@ -15,3 +17,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
         super().integer_validator("width", width)
         super().integer_validator("height", height)
+        try:
+            super().bg.area()
+        except Exception as e:
+            print("[{}] {}".format(e.__class__.__name__, e))
