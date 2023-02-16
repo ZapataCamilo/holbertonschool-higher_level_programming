68381155151c1eff7bcfed4441545b2a3beb5e74
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-16 07:48:09 -0500

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""
-This module contains a function called
-inhertis_from that returns True or False
-"""
+"""function that returns True if the object is an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class ; otherwise False"""
 
 
-def inherits_from(obj, a_class):
-    """
-    This function returns True if the object is
-    and instance of a class that inherited from
-    the specified class; otherwise False
-    """
-    if isinstance(obj, a_class) and type(obj) is not a_class:
-        return True
+def is_kind_of_class(obj, a_class):
+    """Checks if the object is an instance or instance of a class"""
+
+    if isinstance(obj, a_class):
+        return isinstance(obj, a_class)
     else:
         return False
