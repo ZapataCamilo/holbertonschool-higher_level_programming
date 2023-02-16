70b2d1beadfed6bfa266880abdc5fd5691ed098f
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-16 06:30:12 -0500

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -8,6 +8,6 @@ def is_kind_of_class(obj, a_class):
     """Checks if the object is an instance or instance of a class"""
 
     if isinstance(obj, a_class):
-        return True
+        return isinstance(obj, a_class)
     else:
         return False
---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""returns True if the object is an instance of a class that
+inherited (directly or indirectly)
+from the specified class ; otherwise False"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if the object is an  instance of a class that inherited"""
+    if isinstance(obj, a_class):
+        obj = 1
+        return isinstance(obj, a_class)
+    else:
+        return False
