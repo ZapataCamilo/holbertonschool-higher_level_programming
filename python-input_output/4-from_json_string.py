15513632c5d37b6356ebdb36a1740d44a9124b7c
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-17 12:35:49 -0500

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function that returns an object
+"""
+
+
+def from_json_string(my_str):
+    """
+    function that returns an object
+    represented by a JSON string
+    """
+
+    import json
+    return json.loads(my_str)
