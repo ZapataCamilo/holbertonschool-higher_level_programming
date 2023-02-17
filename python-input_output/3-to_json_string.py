42a3ea30e8f868e84c2ccc56ceb5464a8dff1d07
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-17 12:28:54 -0500

    task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import json
 """
  function that returns the JSON
  representation of an object
@@ -7,6 +6,7 @@ import json
 
 
 def to_json_string(my_obj):
+    import json
     """
      returns the JSON
      representation of an object
