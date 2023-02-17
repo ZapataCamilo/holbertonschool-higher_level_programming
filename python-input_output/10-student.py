c92a4f079956f0c4efc0c92694062dec8d06771a
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-17 16:05:21 -0500

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+class Student
+"""
+
+
+class Student:
+    """
+    class Student that defines a student by
+    first_name
+    last_name
+    age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list and all(type(i) == str for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+
+        else:
+            return self.__dict__
