7dc24ef0b8f6ae8ea75d6c03e0bfaa62e4e73a28
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-21 14:45:38 -0500

    task 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -26,9 +26,41 @@ class Rectangle(Base):
         super().__init__(id)
 
     @property
-    def public_set(self):
-        return (self.__width, self.__height, self.__x, self.__y)
+    def public_width(self):
+        """Returns the value of the __width attribute"""
+        return self.__width
 
-    @public_set.setter
-    def public_set(self):
-        return (self.width, self.height, self.x, self.y)
+    @public_width.setter
+    def public_width(self, value):
+        """Sets the value of the __width attribute"""
+        self.__width = value
+
+    @property
+    def public_height(self):
+        """Returns the value of the __width attribute"""
+        return self.__height
+
+    @public_height.setter
+    def public_height(self, value):
+        """Sets the value of the __width attribute"""
+        self.__height = value
+
+    @property
+    def public_x(self):
+        """Returns the value of the __width attribute"""
+        return self.__x
+    
+    @public_x.setter
+    def public_x(self, value):
+        """Sets the value of the __width attribute"""
+        self.__x = value
+
+    @property
+    def public_y(self):
+        """Returns the value of the __width attribute"""
+        return self.__y
+
+    @public_y.setter
+    def public_y(self, value):
+        """Sets the value of the __width attribute"""
+        self.__y = value
