2b50536d74dbb69ee64a7aee79704b49dc9b44ff
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-21 09:50:17 -0500

    task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 class Base:
+    """
+    This class is to now if there are a id in the object or not
+    """
+
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
 
         if id is not None:
