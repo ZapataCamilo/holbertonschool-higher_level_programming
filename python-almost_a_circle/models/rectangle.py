1d6aaed6c92e16f360693ddf43c5e7d611a1a25a
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-21 18:48:48 -0500

    task 3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,10 +28,10 @@ class Rectangle(Base):
         Here we ask if there are id, width and height
         if not put of id one with the private class attribute __nb_objects
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
