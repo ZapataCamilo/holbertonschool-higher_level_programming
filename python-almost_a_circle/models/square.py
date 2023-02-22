511f19474dec4153d2ef84672d97d5e5474c4dea
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-22 18:59:44 -0500

    task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -40,13 +40,13 @@ class Square(Rectangle):
         assigning an argument to each attribute.
         """
         if args:
-            if args[0]:
+            if len(args) >= 1:
                 self.id = args[0]
-            if len(args) == 2 and args[1]:
+            elif len(args) >= 2:
                 self.size = args[1]
-            if len(args) == 3:
+            elif len(args) >= 3:
                 self.x = args[2]
-            if len(args) == 4:
+            elif len(args) =>= 4:
                 self.y = args[3]
 
         elif kwargs:
