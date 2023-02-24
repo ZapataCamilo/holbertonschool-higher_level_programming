55b7363665bfc93f07813ee87e0381b67ff05e69
ZapataCamilo <zapatacamilo86@gmail.com>
2023-02-24 12:21:22 -0500

    task 0

--- python-almost_a_circle/tests/test_models/test_rectangule.py
+++ python-almost_a_circle/tests/test_models/test_rectangule.py
@@ -1,10 +1,14 @@
 from models.rectangle import Rectangle
+from models.base import Base
 import unittest
 
-"""class Rectangle_Test(unittest.TestCase):
+class Rectangle_Test(unittest.TestCase):
     def test_init(self):
         rec = Rectangle(1, 2)
         self.assertEqual(rec.id, 1)
 
-        rec2 = Rectangle(1, 4, 7, 5, 10)
-        self.assertEqual(rec2.id, 10)"""
+        rec2 = Rectangle(1, 4, 7)
+        self.assertEqual(rec2.id, 2)
+
+        rec3 = Rectangle(1, 3, 6, 7)
+        self.assertEqual(rec3.id, 3)
