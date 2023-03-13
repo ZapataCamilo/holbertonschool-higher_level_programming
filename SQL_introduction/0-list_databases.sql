25d54130afcd9fcbc6835c198dd1697aa01689bd
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-13 17:52:22 -0500

    Task 0

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,5 +1,3 @@
 -- script that lists all databases
 SELECT schema_name
-FROM information_schema.schemata
-WHERE schema_name LIKE '%schema' OR 
-      schema_name LIKE '%s';
+FROM information_schema.schemata;  
