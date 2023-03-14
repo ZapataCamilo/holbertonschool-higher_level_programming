777d9595eb2eeecefad6ed05eee1f3c33a6b85e5
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-14 14:44:07 -0500

    Task 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- computes the score average of all records in the table second_table
-SELECT score, COUNT(*) as `name`
+SELECT score, COUNT(*) as `number`
 FROM second_table
 GROUP BY score
 ORDER BY score DESC;
