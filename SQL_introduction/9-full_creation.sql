a9a1b7b2ec7c577046dcf98bc93ce9e1bee32876
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-14 11:40:47 -0500

    Task 9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS second_table (
 
 INSERT INTO second_table
 VALUES
-    (id=1, "John", 10),
-    (id=2, "Alex", 3),
-    (id=3, "Bob", 14),
-    (id=4, "George", 8);
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
+    (4, "George", 8);
