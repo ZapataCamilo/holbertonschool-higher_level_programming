435993204f00a4d751ffe443ff9966a06f4ad667
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-17 17:02:32 -0500

    Task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,29 +4,25 @@ Write a Python file similar to model_state.py
 named model_city.py that contains the class definition of a City
 """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 from model_state import Base, State
-from model_city import City
+from model_city import Base, City
 
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-def list_city_obj():
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(username, password, database),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    rows = session.query(State, City).join(City).all()
-
-    for i in rows:
-        print("{}: ({}) {}".format(i[0].__dict__['name'],
-                                   i[1].__dict__['id'],
-                                   i[1].__dict__['name']))
-
+    for city, state in session.query(City, State).\
+            filter(City.state_id == State.id).\
+            order_by(City.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
