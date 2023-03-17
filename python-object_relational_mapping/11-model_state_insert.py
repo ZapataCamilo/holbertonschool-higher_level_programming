d684d76ce988c47143d7e781b869ec22852a7397
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-17 16:36:07 -0500

    Task 11

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+adds the State object “Louisiana”
+to the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    rows = session.query(State).all()
+    sta = State(name='Louisiana')
+
+    session.add(sta)
+    session.commit()
+    print(sta.id)
+
+    session.close()
