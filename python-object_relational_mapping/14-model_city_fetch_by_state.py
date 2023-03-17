0f72ef4f87814f1d5ab9c347ff1c2c09d53c82dd
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-17 16:54:51 -0500

    Task 14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Write a Python file similar to model_state.py
+named model_city.py that contains the class definition of a City
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(username, password, database),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    rows = session.query(State, City).join(City).all()
+
+    for i in rows:
+        print("{}: ({}) {}".format(i[0].__dict__['name'],
+                                i[1].__dict__['id'],
+                                i[1].__dict__['name']))
+
+    session.close()
