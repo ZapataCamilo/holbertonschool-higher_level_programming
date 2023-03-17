cb5f6a32944d680d6d07566989b49d8f81040f59
ZapataCamilo <zapatacamilo86@gmail.com>
2023-03-17 16:58:03 -0500

    Task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,12 +9,13 @@ from sqlalchemy.orm import Session
 from model_state import Base, State
 from model_city import City
 
-if __name__ == '__main__':
+
+def list_city_obj():
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(username, password, database),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
@@ -25,7 +26,7 @@ if __name__ == '__main__':
 
     for i in rows:
         print("{}: ({}) {}".format(i[0].__dict__['name'],
-                                i[1].__dict__['id'],
-                                i[1].__dict__['name']))
+                                   i[1].__dict__['id'],
+                                   i[1].__dict__['name']))
 
     session.close()
