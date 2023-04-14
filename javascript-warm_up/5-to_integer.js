61129b266e9bc659203a589900870690888ceb3c
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-14 12:21:26 -0500

    Task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -8,5 +8,5 @@ if (!numArg[0]) {
 } else if (isNaN(number) === true) {
   console.log('Not a number');
 } else if (typeof number === 'number') {
-  console.log('My number: ', number);
+  console.log('My number:', number);
 }
