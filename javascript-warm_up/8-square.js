e8c257347026f9c3c9ee04224657888a11d40082
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-14 12:53:05 -0500

    Task 8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,7 +1,9 @@
-#!/bin/usr/node
+#!/usr/bin/node
 
 if (!process.argv[2]) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
+} else if (!parseInt(process.argv[2], 10)) {
+  console.log('Missing size');
 }
 for (let i = 0; i < process.argv[2]; i++) {
   console.log('x'.repeat(process.argv[2]));
