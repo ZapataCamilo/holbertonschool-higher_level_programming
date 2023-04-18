2d67af6ba10b292fd65c9737d8512d56f32249e9
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-18 11:57:11 -0500

    Task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,9 +9,9 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.width; i++) {
+    for (let i = 0; i < this.height; i++) {
       let v = '';
-      for (let j = 0; j < this.height; j++) {
+      for (let j = 0; j < this.width; j++) {
         v += 'X';
       }
       console.log(v);
