b6171979132037f1e460bfe3081ba37bce59ee92
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-19 19:05:53 -0500

    Task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const js = JSON.parse(body).js;
+    const js = JSON.parse(body).results;
     console.log(js.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
