d18d418dc6cb282ccac78ed9c5535859c66cfff3
ZapataCamilo <zapatacamilo86@gmail.com>
2023-04-19 19:00:43 -0500

    Task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const js = JSON.parse(body).js;
+    console.log(js.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
